a428492a2b646c84a1fcd8c3cd2a0e3d589aa0bc
SirJohnSem <jewgienijdanilczik@mail.ru>
2021-09-15 15:33:18 +0200

    add current exchange rates logic

---
+++ src/js/app.js
@@ -0,0 +1,42 @@
+console.log('Hello Web');
+
+const chooseCodeForm = document.querySelector('.page__rates_form');
+const getRatesBtn = document.querySelector('.page__rates_get-rates');
+const tableDate = document.querySelector('#table-date');
+const tableCurrency = document.querySelector('#table-currency');
+const tableBid = document.querySelector('#table-bid');
+const tableAsk = document.querySelector('#table-ask');
+let code;
+
+getRatesBtn.addEventListener('click', function(){
+    getCleanTable();
+    getDataInTable();
+})
+
+function getDataInTable(){
+    getCodeFromRadio();
+    fetch(`https://api.nbp.pl/api/exchangerates/rates/c/${code}/today/?format=json`, {mode:'cors'} )
+      .then(response => response.json())
+      .then(function(response){
+         tableDate.innerHTML = response.rates[0].effectiveDate; 
+         tableCurrency.innerHTML = response.code;
+         tableBid.innerHTML = response.rates[0].bid;
+         tableAsk.innerHTML = response.rates[0].ask;
+      })
+}
+
+function getCodeFromRadio(){
+    code = chooseCodeForm.elements["curr_code"].value;
+}
+
+function getCleanTable(){
+    tableDate.innerHTML = '';
+    tableCurrency.innerHTML = '';
+    tableBid.innerHTML = '';
+    tableAsk.innerHTML = '';
+}
+
+
+
+
+
