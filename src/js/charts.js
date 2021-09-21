f929adeca4ac24a199ff2152b9eff4c390704e13
SirJohnSem <jewgienijdanilczik@mail.ru>
2021-09-21 21:20:36 +0200

    add CHARTS page logic

--- src/js/charts.js
+++ src/js/charts.js
@@ -1,16 +1,70 @@
 import Chart from 'chart.js/auto';
 
-let ctx = document.getElementById('myChart').getContext('2d');
-let myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-    labels: [1,2,3,4,5,6,7],
-    datasets: [{
-    label: 'My First Dataset',
-    data: [65, 59, 80, 81, 56, 55, 40],
-    fill: false,
-    borderColor: 'rgb(75, 192, 192)',
-    tension: 0.1
-  }]
+const chartsPage = (function(){
+ 
+  // get displayed chart after button click
+  const displayChart = (function(){
+    const showChartBtn = document.querySelector('.page__charts_show-charts');
+    showChartBtn.addEventListener('click', getDatasForChart);
+  })();
+  
+  // get datas from API
+  function getDatasForChart(){
+    const chartsCurrency = document.querySelector('#page__charts_currency');
+    const chartsPeriod = document.querySelector('#page__charts_period');
+    fetch(`https://api.nbp.pl/api/exchangerates/rates/a/${chartsCurrency.value}/last/${chartsPeriod.value}/`, {mode:'cors'})
+      .then(response => response.json())
+      .then(function(response){
+        const rates = getRatesArray(response.rates)
+        const period = getPeriodArray(response.rates)
+        getChart(rates, period, chartsCurrency.value)     
+      });
+  }
+  
+  //build chart
+  function getChart(rates, period, currency){
+    const ctx = document.getElementById('myChart').getContext('2d');
+    
+    //do data content check of the chart
+    if (window.myLine !== undefined && window.myLine !== null) {
+      window.myLine.destroy() 
+    };
+
+    window.myLine = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: period,
+        datasets: [{
+          label: `${currency}`,
+          data: rates,
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1
+        }]
+      }
+    })
+  } 
+  
+  function getRatesArray(arr){
+    const ratesArray = [];
+    for(let i=0;i<arr.length; i++){
+      ratesArray.push(arr[i].mid);
     }
-}); 
+    return ratesArray;
+  }
+  
+  function getPeriodArray(arr){
+    const periodArray = [];
+    for(let i=0;i<arr.length; i++){
+      periodArray.push(arr[i].effectiveDate);
+    }
+    return periodArray;
+  }
+  
+})()
+
+
+
+
+ 
+
