b1b55b15cd005996d7a2d449107a81f4df2734af
SirJohnSem <jewgienijdanilczik@mail.ru>
2021-09-23 11:58:59 +0200

    add new styles

--- src/js/dom.js
+++ src/js/dom.js
@@ -10,17 +10,29 @@ linkActual.addEventListener('click', function(){
     pageRates.style.display = 'block';
     pageCharts.style.display = 'none';
     pageHistory.style.display = 'none';
+
+    linkActual.classList.add('_active');
+    linkCharts.classList.remove('_active');
+    linkHistory.classList.remove('_active');
 })
 
 linkCharts.addEventListener('click',function(){
     pageCharts.style.display = 'block';
     pageRates.style.display = 'none';
     pageHistory.style.display = 'none';
+
+    linkCharts.classList.add('_active');
+    linkActual.classList.remove('_active');
+    linkHistory.classList.remove('_active');
 })
 
 linkHistory.addEventListener('click', function(){
     pageHistory.style.display = 'block';
     pageCharts.style.display = 'none';
     pageRates.style.display = 'none';
+
+    linkHistory.classList.toggle('_active');
+    linkActual.classList.remove('_active');
+    linkCharts.classList.remove('_active');
 })
 
